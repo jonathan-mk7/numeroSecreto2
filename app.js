3a878fdab8a7ef9fbdd33e46df083cd006615584
unknown <jonamk07ts@gmail.com>
2026-09-22 13:51:58 -0600

    first com

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Bienvenidos al juego";
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "ingresa un numero del 1 al 10";
+
+
+function intentar(){
+    alert("estoy dandole click al boton intetar ");
+}
