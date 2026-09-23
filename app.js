c0ed7185fce35c90a0ec685710fc8e31d89f76a5
unknown <jonamk07ts@gmail.com>
2026-09-23 13:41:21 -0600

    desafios boton

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Bienvenidos al juego";
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "ingresa un numero del 1 al 10";
 
+let titulo=document.querySelector("h1");
+titulo.innerHTML="Juego del número secreto";
+let parrafo=document.querySelector("p");
+parrafo.innerHTML="Elige un número del 1 al 10";
 
-function intentar(){
-    alert("estoy dandole click al boton intetar ");
+function intentoDeUsuario() {
+    alert ("click desde el boton") 
 }
